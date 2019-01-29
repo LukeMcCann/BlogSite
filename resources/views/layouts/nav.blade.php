5e20668bec029413cab5e6fd3819bd44567a108d
Luke McCann <developer-lukemccann@outlook.com>
2019-01-29 21:58:46 +0000

    Fixes issue navbar above content

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark navbar-laravel">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
+        <a class="navbar-brand" href="{{ url('/index') }}">
             {{ config('app.name', 'Laravel') }}
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -40,7 +40,9 @@
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{url('/profile')}}">Profile</a>
-                            <a class="dropdown-item" href="{{url('/profile')}}">Category</a>
+                            <a class="dropdown-item" href="{{url('/category')}}">Category</a>
+                            <a class="dropdown-item" href="{{url('/post')}}">New-Post</a>
+
                             <hr />
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
--- resources/views/pages/categories/category.blade.php
+++ resources/views/pages/categories/category.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>INDEX</h1>
+CATEGORIES
 @endsection
